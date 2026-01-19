a8d00b52d6930c241e985adc237ac82e1c0b409b
P Mahir Prabha Adhitya <prabhaadhitya01@gmail.com>
2026-01-19 15:29:54 +0000

    setup

---
+++ backend/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+
+const app = express();
+
+app.use(express.json());
+
+const PORT = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Working");
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
